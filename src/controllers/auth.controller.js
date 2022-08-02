0904fff830bcf1efd157e5ef2c9d264cebdaac3c
MSC93 <mcsan2val@gmail.com>
2022-08-02 03:02:59 -0300

    🔥🔥🔥

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,43 @@
-// export const register = async (req, res) => {};
+import { User } from "../models/User.js";
+import jwt from "jsonwebtoken";
 
-// export const login = async (req, res) => {};
+export const register = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-// export const infoUser = async (req, res) => {};
+    const user = new User({ email, password });
+    await user.save();
 
-// export const refreshToken = (req, res) => {};
+    //jwt token
 
-// export const logout = (req, res) => {};
-
-export const register = (req, res) => {
-  console.log(req.body);
-  res.json({ ok: "register true" });
+    return res.status(200).json({ ok: "register save" });
+  } catch (err) {
+    console.log(err.code);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: "Ya existe el usuario" });
+    }
+    return res.status(500).json({ error: "Error de servidor" });
+  }
 };
 
-export const login = (req, res) => {
-  res.json({ ok: "login true" });
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password)))
+      throw new Error("Email or password is incorrect");
+
+    // const { token, expiresIn } = generateToken(user.id);
+    // generateRefreshToken(user.id, res);
+    // return res.json({ token, expiresIn });
+
+    //generar el token jwt
+    const token = jwt.sign({ uid: user._id }, process.env.JWT_SECRET);
+
+    return res.status(200).json({ token });
+  } catch (err) {
+    console.log(err);
+    return res.status(403).json({ error: err.message });
+  }
 };
